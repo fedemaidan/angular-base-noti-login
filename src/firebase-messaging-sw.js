6e2c0750f6fd3e2293ffc86b925920933f07fe64
Federico <fede.maidan@gmail.com>
2019-09-09 20:09:42 -0300

    fix secret

--- src/firebase-messaging-sw.js
+++ src/firebase-messaging-sw.js
@@ -7,7 +7,7 @@ importScripts('https://www.gstatic.com/firebasejs/5.5.0/firebase-messaging.js');
 // Initialize the Firebase app in the service worker by passing in the
 // messagingSenderId.
 firebase.initializeApp({
-  'messagingSenderId': '232289099085'
+  'messagingSenderId': 'SENDER-ID'
 });
 
 // Retrieve an instance of Firebase Messaging so that it can handle background
